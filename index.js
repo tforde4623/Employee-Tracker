9814e918c91d1f7335dc401d61a0197915f5d4f4
tt4623 <sltebr33zy@gmail.com>
2020-12-19 21:39:14 -0600

    added function to call action based on input

--- index.js
+++ index.js
@@ -3,7 +3,46 @@ const mysql = require('mysql'); // maybe just put this in connections.js
 const logo = require('asciiart-logo');
 const config = require('./package.json');
 
-// console.log(logo(config).render());
 
+// when start app display ascii art (text from package.json)
+console.log(logo(config).render());
 
+const actions = [
+  {
+    name: 'action',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      "View Departments",
+      "View Employees",
+      "View Roles",
+      "Add Employee",
+      "Add Department",
+      "Add Role",
+      "Update Employee Roles",
+      "Quit"
+    ]
+  }
+];
+
+// function for calling desired action
+const nextAction = () => {
+  inquirer
+    .prompt(actions)
+    .then(res => {
+      switch (res.action) {
+        case "View Departments":
+          console.log('worked');
+          break;
+        case "View Employees":
+          // prompt function
+          break;
+        default:
+          //something went wrong
+      };
+    })
+    .catch(err => { if (err) throw err; });
+  };
+
+nextAction();
 
