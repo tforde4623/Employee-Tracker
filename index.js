d1fe5bca7d1a0e317897bc30c3513b70f4775303
tt4623 <sltebr33zy@gmail.com>
2020-12-19 19:37:58 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql'); // maybe just put this in connections.js
+const logo = require('asciiart-logo');
+const config = require('./package.json');
+
+// console.log(logo(config).render());
+
+
+
