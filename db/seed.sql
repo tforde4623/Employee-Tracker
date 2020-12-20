2da8870bd8f50308f4040a4e17d178ffc0ecb2b1
tt4623 <sltebr33zy@gmail.com>
2020-12-19 21:39:42 -0600

    added sql files

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+INSERT INTO department(name)
+VALUES
+("Legal"),
+("Production"),
+("Marketing");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("Lawyer", 100000, 1),
+("Legal Team Head", 150000, 1),
+("Marketing Analyst", 70000, 3);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+("John", "Doe", 1, 2),
+("Crystal", "Abby", 2, null);
