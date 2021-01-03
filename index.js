a1a50287178bf89e6df2f4ae2eda43dfd1719f2d
tt4623 <sltebr33zy@gmail.com>
2021-01-03 00:48:04 -0600

    finished most inquirer prompts for menu display options

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const inquirer = require('inquirer');
 const logo = require('asciiart-logo');
 const config = require('./package.json');
+const DbFuncs = require('./db/index.js');
+const cTable = require('console.table');
 
 
 // when start app display ascii art (text from package.json)
 console.log(logo(config).render());
+
+const test = [
+  {
+    name: 'foo',
+    age: 10
+  }, {
+    name: 'bar',
+    age: 20
+  }
+]
 
 const actions = [
   {
@@ -23,30 +35,91 @@ const actions = [
     ]
   }
 ];
-
+// module.exports = new DB(connection);
 // function for calling desired action
 const nextAction = () => {
+  let test = '';
   inquirer
     .prompt(actions)
     .then(res => {
       switch (res.action) {
         case "View Departments":
-          viewDepartments();
+          DbFuncs.viewDepartments();
+          nextAction();
           break;
         case "View Employees":
-          // prompt function
+          DbFuncs.viewEmployees();
+          nextAction();
           break;
         case "View Roles":
-          // prompt function
+          DbFuncs.viewRoles();
+          nextAction();
           break;
         case "Add Employee":
-          // prompt function
+          inquirer
+            .prompt([
+              {
+                name: 'first',
+                message: 'What is the employees first name?',
+                type: 'input',
+                default: null // experiment with this
+              },
+              {
+                name: 'last',
+                message: 'What is this employees last name?',
+                type: 'input'
+              },
+              {
+                name: 'role',
+                message: 'What is the employees role ID?',
+                type: 'input'
+              },
+              {
+                name: 'manager',
+                message: 'What is the employees manager ID?',
+                type: 'input'
+              }
+            ])
+            .then(res => DbFuncs.addEmployee(res))
+            .then(nextAction)
+            .catch(err => { if (err) throw err; });
           break;
         case "Add Department":
-          // prompt function
+          inquirer
+            .prompt({
+              name: 'department',
+              message: 'What is the name of the department you would like to add?',
+              type: 'input'
+            })
+            .then(res => DbFuncs.addDepartment(res.department))
+            .then(nextAction)
+            .catch(err => { if (err) throw err; });
           break;
         case "Add Role":
-          // prompt function
+          inquirer
+            .prompt([
+              {
+                name: 'title',
+                message: 'What is the roles title?',
+                type: 'input',
+                default: null //experiment with this
+              },
+              {
+                name: 'salary',
+                message: 'What is role\'s salary?',
+                type: 'input',
+                default: null //experiment with this
+              },
+              {
+                name: 'depId',
+                message: 'What is the roles id?',
+                type: 'input',
+                default: null //experiment with this
+              }
+            ])
+            .then(res => DbFuncs.addRole(res))
+            .then(nextAction)
+            .catch(err => { if (err) throw err; });
           break;
         case "Update Employee Roles":
           // prompt function
@@ -55,17 +128,13 @@ const nextAction = () => {
           // exit function
           break;
         default:
-          //something went wrong
+        //something went wrong
       };
     })
     .catch(err => { if (err) throw err; });
 };
-  
-// action functions (maybe move some of the functionality into db/)
-const viewDepartments = () => {
-  
-};
-
 
 nextAction();
 
+// maybe for the function do the sql query. then log the table, then next prompt
+// also maybe put function to find choices in choices in prompt (return into choices)
