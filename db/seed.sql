fe50beea84da725e85cf5f625a9462b121516b2e
tt4623 <sltebr33zy@gmail.com>
2021-01-03 01:13:34 -0600

    added seed file to add "test" data into db

--- db/seed.sql
+++ db/seed.sql
@@ -9,7 +9,7 @@ VALUES
 INSERT INTO role(title, salary, department_id)
 VALUES
 ("Lawyer", 100000, 1),
-("Legal Team Head", 150000, 1),
+("Legal Team Head", 150000, 2),
 ("Marketing Analyst", 70000, 3);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
